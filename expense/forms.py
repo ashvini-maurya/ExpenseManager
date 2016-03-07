91dc007286d1d13c637767b7ea284e9358178215
ashvinikumar <ashvinikumar45@gmail.com>
2016-03-07 22:22:23 +0530

    user authorisation

---
+++ expense/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from expense.models import Transaction
+
+class TransactionForm(forms.ModelForm):
+    amount = forms.IntegerField
+    comment = forms.CharField
+    category = forms.CharField
--- expense_manager/urls.py
+++ expense_manager/urls.py
@@ -1,5 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from registration.backends.simple.views import RegistrationView
+
+
+class MyRegistrationView(RegistrationView):
+    def get_success_url(self, request, user):
+        return '/expense/'
+
 
 urlpatterns = patterns('',
     # Examples:
@@ -7,5 +14,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url('^expense/', include('expense.urls')),
+    url(r'^expense/', include('expense.urls')),
+    url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 )
