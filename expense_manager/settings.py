90230b93ec63cf22cd126fdf0ae39d7340eda356
ashvinikumar <ashvinikumar45@gmail.com>
2016-04-16 18:26:13 +0530

    removed secret keys from settings

--- expense_manager/settings.py
+++ expense_manager/settings.py
@@ -92,11 +92,8 @@ MIDDLEWARE_CLASSES = (
 )
 
 
-SOCIAL_AUTH_FACEBOOK_KEY = '1003344473068473'
-SOCIAL_AUTH_FACEBOOK_SECRET = '5ce9bc59594ae94ae49ed769beab031d'
+from settings_secret import *
 
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '293612061387-9dhlbskkjdpfgcjl4fq9kmu3tcrmpdda.apps.googleusercontent.com'
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'hTIxGfkXCGcWW3Mb9Q1tvO5l'
 
 ROOT_URLCONF = 'expense_manager.urls'
 
